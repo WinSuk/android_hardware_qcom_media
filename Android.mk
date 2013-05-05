8c0fc8f67cbb0be4f3c6fcefaff0d08dbf5b9465
WinSuk <goodboy735@gmail.com>
2013-05-05 02:33:29 -0700

    Exclude unwanted stuff (specifically mm-video) on 7x27
    Change-Id: I57c46afc34fde9e83f0529abf28dd23de7f53c0e

--- Android.mk
+++ Android.mk
@@ -1,5 +1,9 @@
 ifneq ($(filter msm8960 msm8660 msm7x30 msm7x27,$(TARGET_BOARD_PLATFORM)),)
 
-include $(call all-subdir-makefiles)
+ifeq ($(TARGET_BOARD_PLATFORM),msm7x27)
+  include $(call all-named-subdir-makefiles,libstagefrighthw mm-core)
+else
+  include $(call all-subdir-makefiles)
+endif
 
 endif
